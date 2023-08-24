245d13b3ab3806620ad843f6b378d72d93bda5dc
SistemasF <c.mujica@fesepsa.pe>
2023-08-24 09:04:40 -0500

    actualizacion vista sistemas

--- crud/sistemasRequerimientos.js
+++ crud/sistemasRequerimientos.js
@@ -6,11 +6,46 @@ from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js"
 
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/+esm'
 //import Swal from 'sweetalert2'
+import 'https://cdn.sheetjs.com/xlsx-0.20.0/package/dist/xlsx.full.min.js'
 
 const btnAbrirModal = document.getElementById('btnAbrirModal');
 const btnCerrarModal = document.getElementById('closeModal');
 const modal = document.getElementById('modal');
 const btnAgregarTarea = document.getElementById("btnAgregarTarea");
+
+const btnTareaPendiente = document.getElementById("btnTareaPendiente");
+const btnTareaCompletada = document.getElementById("btnTareaCompletada");
+
+btnTareaPendiente.onclick = function(){
+     obtenerRequerimientosBDPendientes()
+}
+
+btnTareaCompletada.onclick = function(){
+    obtenerRequerimientosBDCompletados()
+}
+
+
+
+const btnGenerarExcel = document.getElementById("generarExcel");
+btnGenerarExcel.onclick = function(){
+
+  let tabla = document.getElementById("dataExcel")
+  let tablaClonada = tabla.cloneNode(true)
+  let rows = tablaClonada.getElementsByTagName('tr');
+
+      for (let i = 0; i < rows.length; i++) {
+         rows[i].lastElementChild.remove();
+        /*for (let j = 0; j < 2; j++) { // Eliminar las dos últimas celdas
+          rows[i].lastElementChild.remove();
+        }*/
+      }
+
+
+
+  let wb = XLSX.utils.table_to_book(tablaClonada);
+  XLSX.writeFile(wb, "Requerimientos.xlsx");
+}
+
 
 
 
@@ -62,6 +97,7 @@ btnAgregarTarea.addEventListener("click",async()=>{
 <th>Estado</th>
 <th>Accion</th>
 */
+
 
 
 async function obtenerRequerimientosBDPendientes(){
@@ -149,6 +185,96 @@ async function obtenerRequerimientosBDPendientes(){
  }
  
  
+
+
+ async function obtenerRequerimientosBDCompletados(){
+
+    const tbodyRequerimientos = document.getElementById("cajaRequerimientosSistemas")
+   
+    await  leerRequerimientos('sistemasRequerimientos',(requerimientos)=>{
+           limpiarHTML(tbodyRequerimientos);
+           requerimientos.docs.map(requerimientoDB=>{
+                
+                const {requerimiento,metodologia,observacion,fecha,estado} = requerimientoDB.data();
+                if(estado){
+                    
+                    // Crear un objeto Date con la marca de tiempo
+                    const date = new Date(fecha.toDate());
+
+                    // Obtener los componentes de la fecha (día, mes y año)
+                    const dia = date.getDate().toString().padStart(2, '0'); // Asegura que siempre tenga dos dígitos
+                    const mes = (date.getMonth() + 1).toString().padStart(2, '0'); // Los meses son base 0, por eso sumamos 1
+                    const anio = date.getFullYear();
+
+                    // Obtener los componentes de la hora (hora, minuto y segundo)
+                    const hora = date.getHours().toString().padStart(2, '0');
+                    const minutos = date.getMinutes().toString().padStart(2, '0');
+                    const segundos = date.getSeconds().toString().padStart(2, '0');
+
+
+                    // Formatear la fecha en el formato "dd/mm/yyyy"
+                    const fechaFormateada = `${dia}/${mes}/${anio}`;
+
+
+                    // Formatear la hora en el formato "hh:mm:ss"
+                    const horaFormateada = `${hora}:${minutos}:${segundos}`;
+
+                    const filaReq = document.createElement("tr");
+
+                    const tdRequerimiento = document.createElement("td")
+                    const tdMetodologia = document.createElement("td")
+                    const tdObservacion = document.createElement("td")
+                    const tdFecha = document.createElement("td")
+                    const tdHora = document.createElement("td")
+                    const tdEstado = document.createElement("td")
+
+                    const tdbtn = document.createElement("td")
+                    const btnReq = document.createElement("button")
+                    btnReq.classList.add("action-button","action-cambEstado")
+
+                    tdRequerimiento.textContent = requerimiento
+                    tdMetodologia.textContent = metodologia
+                    tdObservacion.textContent = observacion
+                    tdFecha.textContent = fechaFormateada
+                    tdHora.textContent = horaFormateada
+
+                    if(estado){
+                        tdEstado.textContent = "Resuelto"
+                      }else{
+                        tdEstado.textContent = "Pendiente"
+                      }
+                   
+                      btnReq.textContent = "Cambiar Estado"
+                      tdbtn.appendChild(btnReq)
+
+                      btnReq.onclick = async function(){ 
+                        await cambiarEstadoRequerimiento('sistemasRequerimientos',requerimientoDB.id,{
+                         estado:true,
+                         resuelto: serverTimestamp()
+                       })
+                     }
+                    
+                     filaReq.appendChild(tdRequerimiento)
+                     filaReq.appendChild(tdMetodologia)
+                     filaReq.appendChild(tdObservacion)
+                     filaReq.appendChild(tdFecha)
+                     filaReq.appendChild(tdHora)
+                     filaReq.appendChild(tdEstado)
+                     filaReq.appendChild(tdbtn)
+
+                     tbodyRequerimientos.appendChild(filaReq)
+
+                }
+                                
+    
+           })
+      })
+ }
+
+
+
+
+
  obtenerRequerimientosBDPendientes()
  
 
